8c3e33ca3a717a3b49459fd42554342149905a9b
James Laurie <103063112+Jimmy90s@users.noreply.github.com>
2023-02-13 21:34:18 -0800

    Delete degree.h

--- degree.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE };
-
-// Parallel array pops out a string for each type - use the program types an an index
-static const std::string degreeTypeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
